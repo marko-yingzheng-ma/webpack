b44bdb54ca30b4d8e68a6497a590a5dbee7249ff
Yingzheng Ma <yma39@ford.com>
2019-07-03 13:58:14 -0700

    set up bare-bone webpack

---
+++ js_modules/src/sum.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+/**
+ *  Module System |  Common Syntax
+ * ---------------|----------------------------------------
+ * CommonJS(node) |   module.exports, require
+ *    AMD         |   define, require
+ *  ES2015 (es6)  |   export, import
+ */
+
+ module.exports = sum;
