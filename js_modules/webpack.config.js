11015fe3b33ab5d034768a3e101fb7ae629ee1dc
Marko Ma <yma004@drury.edu>
2019-07-05 12:53:23 -0700

    added comments

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
-
+/**
+ *    Module        |   Purpose
+ *  ----------------|---------------
+ *  babel-loader    |   Teaches babel how to work with webpack
+ *  babel-core      |   Knows how to take in code, parse it, and generate some output files
+ *  babel-preset-env|   Ruleset for telling babel exactly what pieces of ES6/7/8 syntax to look for, and how to turn it into ES5 code
+ */
 const config = {
   entry: './src/index.js',
   output: {
@@ -9,8 +15,9 @@ const config = {
   module: {
     rules: [
       {
+        // do preprocessing before webpack do the work linking source code into bundle.js
         use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/ //only process .js files
       }
     ]
   }
