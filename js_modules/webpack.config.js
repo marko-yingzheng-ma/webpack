a682671d9d906d53c14fa9883d5ebfc85fd16a1c
Marko Ma <yma004@drury.edu>
2019-07-05 15:00:38 -0700

    added css and style loader to read, parse css in bundle.js and inject it to style tags in html

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -5,6 +5,9 @@ const path = require('path');
  *  babel-loader    |   Teaches babel how to work with webpack
  *  babel-core      |   Knows how to take in code, parse it, and generate some output files
  *  babel-preset-env|   Ruleset for telling babel exactly what pieces of ES6/7/8 syntax to look for, and how to turn it into ES5 code
+ * 
+ *  css-loader      |   Knows how to deal with CSS imports (open the file and read the content).
+ *  style-loader    |   Takes the CSS imports (content) and adds them to the HTML document under <style> tag.
  */
 const config = {
   entry: './src/index.js',
@@ -18,6 +21,10 @@ const config = {
         // do preprocessing before webpack do the work linking source code into bundle.js
         use: 'babel-loader',
         test: /\.js$/ //only process .js files
+      }, 
+      {
+        use: ['style-loader', 'css-loader'], // order matters, interprets from RIGHT to LEFT
+        test: /\.css$/
       }
     ]
   }
