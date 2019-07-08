321c8926cead6a08e92e9b19c081388c87c1e964
Yingzheng Ma <yma39@ford.com>
2019-07-08 10:58:47 -0700

    added extractCss plugin to take any files transformed/processed by css-loader and save it into style.css

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 /**
  *    Module        |   Purpose
  *  ----------------|---------------
@@ -9,6 +11,18 @@ const path = require('path');
  *  css-loader      |   Knows how to deal with CSS imports (open the file and read the content).
  *  style-loader    |   Takes the CSS imports (content) and adds them to the HTML document under <style> tag.
  */
+
+ 
+ /**
+  *   Difference between Loaders and Plugins: 
+  *
+  *   Loaders: work at the individual file level during or before the bundle is generated.
+  *   Plugins: work at bundle or chunk level and usually work at the end of the bundle generation process. Plugins can also modify how the bundles themselves are created. 
+  *      Plugins have more powerful control than loaders.
+  * 
+  *   https://stackoverflow.com/questions/37452402/webpack-loaders-vs-plugins-whats-the-difference
+  */
+
 const config = {
   entry: './src/index.js',
   output: {
@@ -23,11 +37,22 @@ const config = {
         test: /\.js$/ //only process .js files
       }, 
       {
-        use: ['style-loader', 'css-loader'], // order matters, interprets from RIGHT to LEFT
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }, 
+          'css-loader'
+        ],
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // tells MiniCssExtractPlugin library to take any files transformed/processed by css-loader and save it into style.css 
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 };
 
 module.exports = config
